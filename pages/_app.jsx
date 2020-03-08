1f541e35c4f5774bc579a2e2d32b9312bfea57d8
Duc Tho Tran <ducthotran2010@gmail.com>
2020-03-08 19:26:07 +0700

    Fix build

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import App from 'next/app';
+import '../node_modules/antd/dist/antd.css';
 import '../css/tailwind.css';
 
 class MyApp extends App {
   render() {
+    console.log('hi');
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps}/>;
+    return <Component {...pageProps} />;
   }
 }
 
